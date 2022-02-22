2b580f88b5cf21c49453cf7227e19072fb1d79bb
Robert Granja <robeertgr@live.com>
2022-02-22 10:30:32 -0300

    Exemplo switch case

---
+++ src/Condicao_Ternaria.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Condicao_Ternaria {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Informe o preço do produto: ");
+        double preco = sc.nextDouble();
+
+        double desconto = (preco < 20.0) ? preco * 0.1 : preco * 0.05;
+        System.out.println("Desconto do produto custando R$ " + preco + " foi de R$ " + desconto);
+
+    }
+}
